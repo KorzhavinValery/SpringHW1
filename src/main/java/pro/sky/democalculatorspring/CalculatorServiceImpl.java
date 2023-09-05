b819af40e9705bd038f0579735827c6b3d1eeec6
Vel <korzhavin.vel@yandex.ru>
2023-09-05 16:26:13 +0700

    Spring Calculator

---
+++ src/main/java/pro/sky/democalculatorspring/CalculatorServiceImpl.java
@@ -0,0 +1,7 @@
+package pro.sky.democalculatorspring;
+
+import org.springframework.stereotype.Service;
+
+public class CalculatorServiceImpl implements CalculatorService {
+
+}
---
+++ src/main/java/pro/sky/democalculatorspring/DemocalculatorspringApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.democalculatorspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocalculatorspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocalculatorspringApplication.class, args);
+	}
+
+}
