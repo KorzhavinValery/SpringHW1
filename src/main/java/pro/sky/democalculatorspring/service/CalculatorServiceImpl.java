a60eb99bd535ecaf7054246ab083b65c201e26f9
Vel <korzhavin.vel@yandex.ru>
2023-09-06 15:57:12 +0700

    Spring Calculator исправления

---
+++ src/main/java/pro/sky/democalculatorspring/service/CalculatorServiceImpl.java
@@ -0,0 +1,36 @@
+package pro.sky.democalculatorspring.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+    private int sum;
+
+
+
+
+    public int plusCalculator(int num1, int num2) {
+        sum = num1 + num2;
+        return sum;
+    }
+
+    public int minusCalculator(int num1, int num2) {
+        sum = num1 - num2;
+        return sum;
+    }
+
+    public int multiplyCalculator(int num1, int num2) {
+        sum = num1 * num2;
+        return sum;
+    }
+
+    public int divideCalculator(int num1, int num2) {
+        if (num1 != 0 && num2 != 0) {
+            sum = num1 / num2;
+            return sum;
+        } else return Integer.parseInt("<b>На 0 делить нельзя!</b>");
+
+    }
+
+
+}
